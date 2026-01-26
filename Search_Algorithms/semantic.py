b9614df86f1849112d509065051f7b948e8b89b0
HJfod <dreadrollmusic@gmail.com>
2026-01-26 14:31:07 +0200

    reorganize + use SemanticSearchEngine in week3 ipynb

--- Search_Algorithms/semantic.py
+++ Search_Algorithms/semantic.py
@@ -15,21 +15,22 @@ class SemanticSearchEngine:
         text_without_stopwords = gensim.parsing.preprocessing.remove_stopwords(text) # type: ignore
         tokens = gensim.utils.simple_preprocess(text_without_stopwords) # type: ignore
         return self.ft.get_mean_vector(tokens)
-
-    def __init__(self, documents: list[str]) -> None:
+    
+    @staticmethod
+    def install_embeddings() -> typing.Any:
         print("Loading dataset, this will take a while")
-        self.ft = gsapi.load("fasttext-wiki-news-subwords-300") # type: ignore
+        return gsapi.load("fasttext-wiki-news-subwords-300") # type: ignore
+
+    def __init__(self, ft: typing.Any, documents: list[str]) -> None:
+        self.ft = ft
         self.documents = documents
         self.doc_vectors = KeyedVectors(self.ft.vector_size, count=len(documents))
         for i, doc in enumerate(documents):
             dv = self.into_wordvec(doc)
             self.doc_vectors.add_vector(i, dv) # type: ignore
 
-    def search(self, query: str) -> list[str]:
+    def search(self, query: str, topn: int = 5) -> list[str]:
         q = self.into_wordvec(query)
-        most_sim = self.doc_vectors.most_similar([q], topn=5) # type: ignore
+        most_sim = self.doc_vectors.most_similar([q], topn=topn) # type: ignore
 
-        for doc_position, doc_score in most_sim: # type: ignore
-            print(f"- {doc_score:.4f}  ({doc_position:>3})  {self.documents[doc_position][:100]}...")
-        
-        return []
+        return [self.documents[p] for p, _ in most_sim] # type: ignore
